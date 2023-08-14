937b42a84b349fdde001aca1bd34e9a61b255bc5
dvegas03 <87619555+dvegas03@users.noreply.github.com>
2023-08-14 00:53:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     #Flask app for health checks
     from threading import Thread
 
-    flask_thread = Thread(target=webapp.run, kwargs={'port': 8080})
+    flask_thread = Thread(target=webapp.run, kwargs={'host': '0.0.0.0', 'port': 8080})
     flask_thread.start()
 
     try:
